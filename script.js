b49cea15e66b2b66b73232171ffdda055dde8468
Toye Ogundepo <ogundepo@gmail.com>
2020-08-28 12:17:36 -0500

    reversed content order

--- script.js
+++ script.js
@@ -11,11 +11,6 @@ const dates = [start_date, start_date1, start_date2, start_date3, start_date4, e
 const reversedDates = dates.reverse();
 console.log(reversedDates)
 
-// console.log(start_date)
-// console.log(end_date);
-
-
-
 const nasaImg = Array.from(document.getElementsByClassName("nasa-img"));
 const cardTitle = document.querySelectorAll(".card-title");
 const cardText = document.querySelectorAll(".card-text");
@@ -37,12 +32,11 @@ const reversedCardTitle = cardTitleToArray.reverse();
 const dateToArray = Array.from(date);
 const reversedDate = dateToArray.reverse();
 
-// console.log(cardMediaSection)
+const cardTextToArray = Array.from(cardText);
+const reversedCardText = cardTextToArray.reverse();
 
-
-
-
-// console.log(nasaImg);
+const learnMoreBtnToArray = Array.from(learnMoreBtn);
+const reversedLearnMoreBtnToArray = learnMoreBtnToArray.reverse();
 
 console.log(start_date);
 console.log(start_date1);
@@ -56,93 +50,39 @@ async function fetchImages() {
   .then(res => res.json())
   .then(res => {
     for (let i = 0; i < res.length; i++) {
-    // console.log(res[i].hdurl)
     }
     for (let j = 0; j < nasaImg.length; j++) {
         nasaImg[j].src = res[j].hdurl;
-        // console.log(nasaImg[j].src)
-
         if (nasaImg[j].src.includes("undefined") === true) {
           console.log("this is an iframe");
           const iframe = document.createElement("iframe");
           iframe.src = res[j].url;
           cardMediaSection[j].prepend(iframe)
           cardMediaSection[j].children[1].style.display = "none";
-          // for (let z = 0; z < cardMediaSection.length; z++) {
-            
-          // console.log(iframe)
-         
-          // }
-
-          
-          // console.log(iframe)
-          // console.log(res[j].url)
         }
     }
-
-    
-
     for (let k = 0; k < reversedCardTitle.length; k++) {
-      // console.log(cardTitle[k]);
       reversedCardTitle[k].textContent = `${res[k].title}`
     }
-
-    for (let l = 0; l < cardText.length; l++) {
-      // console.log(cardText[l]);
-      cardText[l].textContent = `${res[l].explanation}`.substring(0, 75) + "...";
+    for (let l = 0; l < reversedCardText.length; l++) {
+      reversedCardText[l].textContent = `${res[l].explanation}`.substring(0, 75) + "...";
     }
-
-    for (let m = 0; m < learnMoreBtn.length; m++) {
-      learnMoreBtn[m].addEventListener("click", function() {
-        moreInfoText.textContent = `${res[m].explanation}`
+    for (let m = 0; m < reversedLearnMoreBtnToArray.length; m++) {
+      reversedLearnMoreBtnToArray[m].addEventListener("click", function() {
+        moreInfoText.textContent = `${res[m].explanation}`;
         modalTitleText.textContent = `${res[m].title}`;
       })   
     }
-
     for (let i = 0; i < reversedDates.length; i++) {
       for (let j = 0; j < reversedDate.length; j++) {
         reversedDate[i].textContent = reversedDates[i]
       }
     }
-
-
-    //Button Text
-    // for (let n = 0; n < learnMoreBtn.length; n++) {
-    //   learnMoreBtn[n].innerHTML = "Learn more";
-    // }
-
-    // for (let q = 0; q < cardMediaSection.length; q++) {
-    //   // if (cardMediaSection[q].)
-    //   console.log(cardMediaSection[q].children)
-    // }
-
-    // console.log(res)
-
-    // for (let x = 0; x < cardMediaSection.length; x++) {
-    //   const children = cardMediaSection[x].children
-    //   console.log(children)
-    //   for (let y = 0; y < children.length; y++) {
-    //     console.log(children[y].outerHTML)
-    //     if (children[y].outerHTML.includes("undefined") === true) {
-    //       console.log(y, "is undefined")
-    //       const iframe = document.createElement("iframe");
-    //       iframe.src = res[y].url
-    //       children[y].appendChild(iframe);
-    //       console.log(res[y].url)
-    //     }
-    //   }
-    // }
-  // reversedCard;
-    
   });
 }
 
-
 fetchImages();
 
-
-
-// nasaImg.forEach(item => item.src = `${res[i].hdurl}`)
 
 
 
